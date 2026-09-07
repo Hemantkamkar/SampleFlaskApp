2e2ea4a45e02528b6ed4802aaf8faf09177f534e
Hemantkamkar <hemantkamkar024@gmail.com>
2026-09-07 09:44:31 +0530

    updated v3

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        return 'Hurray GFG - Sudanshu! v2'
+        return 'Hurray GFG - Sudanshu! v3'
 
     @app.route('/test')
     def test():
